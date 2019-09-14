c8ac0685061610c73c53f41d899d71a77ab0ee07
Otshepeng Morake <44570478+tsaona@users.noreply.github.com>
2019-09-14 09:12:57 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php require "header.php"; ?>
+	</head>
+	<body>
+		<!-- Navigation Bar  -->
+		<?php require "navbar.php"; ?>
+		<!--  -->
+		<div class="container-fluid">
+		 	<!-- Mission, Vission, Objective -->
+			<div class="statements">
+				<div class="row">
+					<div class="col-6" id="Mission"></div>
+					<div class="col-6" id="Vission"></div>
+					<div class="col-8-offset-2" id="Objective"></div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
