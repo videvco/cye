3ca63944a55655367f83f8225974554f8714d7c4
Ofentse Moeketsi Mogoatlhe <u14254922@tuks.co.za>
2019-09-14 11:29:14 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<?php
-		require 'header.php';
-	?>
-</head>
-<body>
-	<?php
-		require 'navbar.php';
-	?>
-</body>
+<html lang = "en">
+	<head>
+		<meta charset = "utf-8"/>
+		<meta name = "author" content = "videv.codes"/>
+		<link rel = "stylesheet" href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+		<link rel = "stylesheet" href = "style.css">
+		<title>Home</title>
+	</head>
+	<body>
+		<div class = "container-fluid containerdiv">
+			<div class = "homeimage">
+				<div class = "hometext-container text-right" class = "col-1">
+					<p class = "hometext text-center font-weight-bold name">Cosmo Youth Empowerment</p>
+					<p class = "hometext text-center mission">A missional non profit organisation that aims to empower youth across the world</p>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
