47b34243fa7be1f120bfd50365a3599b7b371692
Otshepeng Morake <44570478+tsaona@users.noreply.github.com>
2019-09-16 20:13:06 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,39 @@
+$(()=>{
+	(()=>{
+		let container = $('#Objective div.row');
 
+		getInfo('objective.json')
+		.then((data) => {
+			data.forEach((el)=>{
+				objective.append(
+					$('<div></div>')
+				)
+			});
+		});
+	})();
+});
+
+let createObjective = () => {
+	
+	let statement_tree = $('<div></div>')
+						 .addClass('col-12 col-md-6')
+						 .append(
+						 	$('<div></div>')
+						 	.addClass('row mx-0')
+						 	.append(
+						 		$('<div></div>')
+						 		.addClass()
+						 		.append(
+						 			$('<div></div>')
+						 			.addClass('bullet-point col-1 pr-0')
+						 			.append(
+						 				$('<span></span>')
+						 				.html('...'),
+						 				$('<p></p>')
+						 				.addClass('statement-text col pl-0')
+						 			)
+						 		)
+						 	)
+						 )
+	return statement_tree;
+};
